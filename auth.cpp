b2f11469ac539e3f6d81a9874ddbcc3d8e7f2276
ceverger <74200769+ceverger@users.noreply.github.com>
2024-03-14 17:51:08 +0300

    Add files via upload

--- auth.cpp
+++ auth.cpp
@@ -1,14 +1,6 @@
-#include <map>
-#include <list>
 #include <string>
 #include <iostream>
 #include <fstream>
-
-#define bufsize 4096
-#define Login "admin"
-#define Password "admin"
-
-	// void jsonParser(const std::string & json);
 
 	int main(int argc, char *argv[])
 	{
@@ -16,7 +8,7 @@
 		std::ifstream fin;
 		std::ofstream fout;
 
-		std::cin >> str;	
+		std::cin >> str; // Get json from web-server	
 
 		fout.open("auth.txt", std::ios_base::trunc | std::ios_base::out);
 		if(!fout.is_open())
@@ -25,48 +17,13 @@
 			std::cout << "{'status':'false'}\n";
 		}
 
-		fout << str << '\n';
+		fout << str << '\n'; // Write json in auth.txt
 		fout.close();		
 		
+		// Send json to web-server
 		std::cout << "Content-type: application/json\n\n";
 		std::cout << "{\"status\":\"true\"}\n";
 
 		return 0;
 	}
 
-/*	void jsonParser(const std::string & json)
-	{
-		std::size_t posBegin = 0;
-		std::size_t posEnd = 0;
-		list<std::size_t> colons;
-
-		posBegin = json.find('{');
-		if(posBegin == std::string::npos)
-		{
-			std::cout << "Content-type: application/json\n\n";
-			std::cout << "{ status: 'parser_error!' }\n";
-			return 1;				
-		}
-
-		posEnd = json.find('}');
-		if(posEnd == std::string::npos)
-		{
-			std::cout << "Content-type: application/json\n\n";
-			std::cout << "{ status: 'Parser error!' }\n";
-			return 1;				
-		}
-
-		if(posEnd < posBegin)
-		{
-			std::cout << "Content-type: application/json\n\n";
-			std::cout << "{ status: 'Parser error!' }\n";
-			return 1;				
-		}
-
-		for(std::size_t i = posBegin; i < posEnd; ++i)
-		{
-			if(json[i] == ':') colons << i;
-		}
-	}
-
-*/
